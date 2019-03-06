d8c087ce1708c10fc83bd9c6cd0ceec347ababaf
Isaac Falkenstine <isaac@falkenstine.com>
2019-03-06 15:49:14 -0700

    Make board, moving paddle, moving ball and start collision detection

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/javascripts/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    // rules: [{
+    //        test: /\.scss$/,
+    //        use: [
+    //            "style-loader", // creates style nodes from JS strings
+    //            "css-loader", // translates CSS into CommonJS
+    //            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+    //        ]
+    //    }],
+
+    // loaders: [
+    //   { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+    //   { test: /\.css$/, loader: "index!css" },
+    //   { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+    // ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.scss']
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  }
+};
